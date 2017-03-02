c5327abcbd88650ad696cbc4f2212f2a33ef1003
abianco3 <anthony.bianco3@gmail.com>
2017-03-02 14:41:51 -0800

    Serve all static files

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -14,8 +14,17 @@ exports.serveAssets = function(res, asset, callback) {
   // Write some code here that helps serve up your static files!
   // (Static files are things like html (yours or archived from others...),
   // css, or anything that doesn't change often.)
+  var type = path.extname(asset).slice(1);
+  fs.readFile(archive.paths.siteAssets + asset, function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/' + type});
+    res.end(data);
+  });
 };
 
+exports.serveSite = function(res, siteUrl, callback) {
+  res.writeHead(200, exports.headers);
+  res.end(siteUrl);
+};
 
 
 // As you progress, keep thinking about what helper functions you can put here!
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,13 +1,32 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
 var fs = require('fs');
+var utils = require('../web/http-helpers');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  fs.readFile(archive.paths.siteAssets + '/index.html', function(err, data) {
-    archive.readListOfUrls();
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end(data);
-  });
+    
+  if (req.url === '/') {
+    if (req.method === 'POST') {
+      // do some thing
+      var url = '';
+      req.on('data', function(chunk) {
+        url += chunk;
+        console.log(chunk);
+      });
+      req.on('end', function(err) {
+        if (err) {
+          console.error(err);
+        } else {
+          utils.serveSite(res, url);
+        }
+      });
+    } else {
+      utils.serveAssets(res, '/index.html');
+    }
+  } else if (req.url === '/styles.css') {
+    utils.serveAssets(res, '/styles.css');
+  }
+
   //res.end(archive.paths.list);
 };
